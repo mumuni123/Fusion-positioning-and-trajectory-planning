a7f816025cdd94ee2af4cadf521d05e0cdb3e1b6
mumuni123 <126955690+mumuni123@users.noreply.github.com>
2025-02-22 13:34:56 +0800

    Add files via upload

---
+++ INS-GNSS-EKF/psinsenvi.m
@@ -0,0 +1,4 @@
+function [rpath, dpath, mytestflag] = envi()
+	rpath = 'G:\毕业设计\INS-GNSS-EKF';
+	dpath = 'G:\毕业设计\INS-GNSS-EKF\data\';
+	mytestflag = 0;
---
+++ INS-GNSS-EKF/senvi.m
@@ -0,0 +1,4 @@
+function [rpath, dpath, mytestflag] = psinsenvi()
+	rpath = 'G:\毕业设计\INS-GNSS-EKF';
+	dpath = 'G:\毕业设计\INS-GNSS-EKF\data\';
+	mytestflag = 0;
